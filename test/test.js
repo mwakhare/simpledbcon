f600e048ada1beecab1099ad6ecfde3f6c29d8ae
mwakhare <mwakhare@torinit.com>
2017-02-01 17:04:21 +0530

    test case usergetall

--- test/test.js
+++ test/test.js
@@ -5,21 +5,38 @@ var tbleUserModule = require ("../tblUserModule");
 
 var should = chai.should();
 
-describe('User Model', function(){
+describe('User Table Model', function(){
 	describe('Function: tblUserGetOne()', function(){
 		it('should have all properties and return correct id', function( done )
 		{
-			tbleUserModule.tblUserGetOne (1, function(err,rows,fields){
+			tbleUserModule.tblUserGetOne (3, function(err,rows,fields){
 				if( !err ) {
 					rows.should.be.a('array');
 					rows[0].should.have.property('id');
-					//rows[0].should.have.property('name');
-					rows[0].should.have.property('id').equal(1);
-					done();
+					rows[0].should.have.property('user_info');
+					rows[0].should.have.property('id').equal(3);
+					done ();
 				}
 			});
 		});
 	});
 });
 
+describe('User Table Model', function(){
+	describe('Function: tblUserGetAll ()', function(){
+		it('should have all properties and return all User records', function( done )
+		{
+			tbleUserModule.tblUserGetAll (function(err, rows, fields){
+				if( !err ) {
+					rows.should.be.a('array');
+					
+					rows[0].should.have.property('id');
+					rows[0].should.have.property('user_info');
+					
+					done ();
+				}
+			});
+		});
+	});
+});
 
