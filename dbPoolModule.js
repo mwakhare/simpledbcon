5a414169202bb0e9b63329fb5876726cf1efb53f
mwakhare <mwakhare@torinit.com>
2017-02-02 13:25:59 +0530

    after insert

--- dbPoolModule.js
+++ dbPoolModule.js
@@ -1,6 +1,15 @@
-var mysql = require ('mysql');
+/*
+ * module:  dbPoolModule 
+ * -------------------------
+ * This module exports a pool of 'korsall' database connection objects
+ * 
+ */
 
-var pool = mysql.createPool 
+'use strict';
+
+const mysql = require ('mysql');
+
+const pool = mysql.createPool 
 ({
     connectionLimit : 100, //max limit to fix.
     host     : 'localhost',
@@ -8,5 +17,14 @@ var pool = mysql.createPool
     password : 'Tori@2016',
     database : 'korsall'
 });
+
+// var pool = mysql.createPool 
+// ({
+//     connectionLimit : 100, //max limit to fix.
+//     host     : 'localhost',
+//     user     : 'root',
+//     password : 'torinit@123',
+//     database : 'demo'
+// });
 
 module.exports = pool;
--- tblUserModule.js
+++ tblUserModule.js
@@ -1,37 +1,17 @@
 'use strict';
 
-// var mysql = require ('mysql');
-
-// var pool = mysql.createPool 
-// ({
-//     connectionLimit : 100, //max limit to fix.
-//     host     : 'localhost',
-//     user     : 'milind',
-//     password : 'Tori@2016',
-//     database : 'korsall'
-// });
-
-// var pool = mysql.createPool 
-// ({
-//     connectionLimit : 100, //max limit to fix.
-//     host     : 'localhost',
-//     user     : 'root',
-//     password : 'torinit@123',
-//     database : 'demo'
-// });
-
-var pool = require ('./dbPoolModule');
+const pool = require ('./dbPoolModule');  //to get the 'korsall' database connections' pool
 
 /*
  * module:  tblUserModule 
  * -------------------------
  * This module exports 5 database releted fuctions on the 'User' Table
  * 
- *  tblUserGetOne - to get specific user recrod
- *  tblUserGetAll - to get all user recrods
- *  tblUserInsert - to insert specific user recrod
- *  tblUserUpdate - to update specific user recrod
- *  tblUserDelete - to delete specific user recrod
+ * tblUserGetOne - to get specific user id recrod
+ * tblUserGetAll - to get all the user recrods
+ * tblUserInsert - to insert new user recrod
+ * tblUserUpdate - to update specific user id recrod
+ * tblUserDelete - to delete specific user id recrod
  * 
  */
 module.exports = 
@@ -46,7 +26,7 @@ module.exports =
  * callback: callback async fuction 
  *
  * returns:  database record of given user id
- *           returns error (if record not found)
+ *           returns error (if record is not found)
  */
     tblUserGetOne: function (userIdToFind, callback)
     {
@@ -74,9 +54,6 @@ module.exports =
 
                 if (!err) 
                 {
-                    // console.log (rows);
-                    // console.log ("Data received from Database: \n" + rows);
-                    // console.log (fields);   // fields contains extra meta data about results, if available
                     // return (rows);          //specific user id record has found and returned.
                     callback (null, rows, fields);
                 }           
@@ -100,9 +77,8 @@ module.exports =
  * callback: callback async fuction 
  *
  * returns:  all database records (user table)
- *           returns error (if record not found)
+ *           returns error (if records are not found)
  */
-
     tblUserGetAll: function (callback)
     {
         pool.getConnection (function (err, connection)
@@ -129,9 +105,6 @@ module.exports =
 
                 if (!err) 
                 {
-                    // console.log (rows);
-                    // console.log ("Data received from Database: \n" + rows);
-                    // console.log (fields);   // fields contains extra meta data about results, if available
                     // return (rows);          //specific user id record has found and returned.
                     callback (null, rows, fields);
                 }           
@@ -146,6 +119,146 @@ module.exports =
      });
  },
 
+/*
+ * Function:  tblUserDelete 
+ * -------------------------
+ * This function deletes the specific user id recrod from the database
+ *
+ * userIdToDelete: user id whose record to delete from database
+ * callback: callback async fuction 
+ *
+ * returns:  ?
+ *           ? returns zero on error or json (if record is not found)
+ */
+    tblUserDelete: function (userIdToDelete, callback)
+    {
+        pool.getConnection (function (err, connection)
+        {
+            if (err) 
+            {
+                connection.release ();
+                res.json ({"code" : 100, "status" : "Error in database connection"});  //dummy json error code (error code structure to fix)
+                return;
+            }   
+ 
+            console.log ('Database connection thread id: ' + connection.threadId);
+         
+            connection.query ("DELETE FROM user WHERE id = ?",  [userIdToDelete], function (err, result)
+            {
+                connection.release ();
+                if (err) 
+                {   
+                    console.error (err);
+                    callback (err);
+                    return;
+                }   
+
+                if (!err) 
+                {
+                   // console.log ("Data deleted from Database: \n" + result);
+                   // return (result);         
+
+                    // if (result.affectedRows == 1) 
+                    // {
+                    //     output = 
+                    //     {
+                    //         code: 200,
+                    //         msg: 'Record is deleted successfully!'
+                    //     };
+                        
+                    // } 
+                    // else 
+                    // {
+                    //     output = 
+                    //     {
+                    //         code: 500,
+                    //         msg: 'Record is NOT deleted.'
+                    //     }
+                    // }
+
+                    callback (null, result);
+                }           
+            });
+ 
+            //database connection error
+            connection.on ('error', function (err)
+            {      
+                res.json ({"code" : 100, "status" : "Error in database connection"}); //dummy json error code (error code structure to fix)
+                return;     
+            });
+     });
+ },
+
+
+/*
+ * Function:  tblUserInsert 
+ * -------------------------
+ * This function adds a new user recrod in the database
+ *
+ * newUserToAdd: new user object to add in the database
+ * callback: callback async fuction 
+ *
+ * returns:  ?
+ *           ? returns zero on error or json (if record is not inserted)
+ */
+tblUserInsert: function (newUserToAdd, callback)
+    {
+        pool.getConnection (function (err, connection)
+        {
+            if (err) 
+            {
+                connection.release ();
+                res.json ({"code" : 100, "status" : "Error in database connection"});  //dummy json error code (error code structure to fix)
+                return;
+            }   
+ 
+            console.log ('Database connection thread id: ' + connection.threadId);
+         
+            connection.query ("INSERT INTO user SET ?",  [newUserToAdd], function (err, result)
+            {
+                connection.release ();
+                if (err) 
+                {   
+                    console.error (err);
+                    callback (err);
+                    return;
+                }   
+
+                if (!err) 
+                {
+                   // console.log ("Data deleted from Database: \n" + result);
+                   // return (result);         
+
+                    // if (result.affectedRows == 1) 
+                    // {
+                    //     output = 
+                    //     {
+                    //         code: 200,
+                    //         msg: 'Record is deleted successfully!'
+                    //     };
+                        
+                    // } 
+                    // else 
+                    // {
+                    //     output = 
+                    //     {
+                    //         code: 500,
+                    //         msg: 'Record is NOT deleted.'
+                    //     }
+                    // }
+
+                    callback (null, result);
+                }           
+            });
+ 
+            //database connection error
+            connection.on ('error', function (err)
+            {      
+                res.json ({"code" : 100, "status" : "Error in database connection"}); //dummy json error code (error code structure to fix)
+                return;     
+            });
+     });
+ },
 
 // var sql = "SELECT * FROM ?? WHERE ?? = ?";
 // var inserts = ['users', 'id', userId];
@@ -219,76 +332,5 @@ module.exports =
 //     });
 // },
 
-
-/*
- * Function:  tblUserDelete 
- * -------------------------
- * This function deletes the specific user id recrod from the database
- *
- * userIdToDelete: user id whose record to delete from database
- * callback: callback async fuction 
- *
- * returns:  ?
- *           ? returns zero on error or json (if record not found)
- */
-    tblUserDelete: function (userIdToDelete, callback)
-    {
-        pool.getConnection (function (err, connection)
-        {
-            if (err) 
-            {
-                connection.release ();
-                res.json ({"code" : 100, "status" : "Error in database connection"});  //dummy json error code (error code structure to fix)
-                return;
-            }   
- 
-            console.log ('Database connection thread id: ' + connection.threadId);
-         
-            connection.query ("DELETE FROM user WHERE id = ?",  [userIdToDelete], function (err, result)
-            {
-                connection.release ();
-                if (err) 
-                {   
-                    console.error (err);
-                    callback (err);
-                    return;
-                }   
-
-                if (!err) 
-                {
-                    // console.log (record);
-                    // console.log ("Data deleted from Database: \n" + record);
-                   // return (result);          //specific user id record has found and returned.
-
-                    if (result.affectedRows == 1) 
-                    {
-                        output = 
-                        {
-                            code: 200,
-                            msg: 'Record is deleted successfully!'
-                        };
-                        
-                    } 
-                    else 
-                    {
-                        output = 
-                        {
-                            code: 500,
-                            msg: 'Record is NOT deleted.'
-                        }
-                    }
-
-                    callback (null, result);
-                }           
-            });
- 
-            //database connection error
-            connection.on ('error', function (err)
-            {      
-                res.json ({"code" : 100, "status" : "Error in database connection"}); //dummy json error code (error code structure to fix)
-                return;     
-            });
-     });
- },
 
 }
