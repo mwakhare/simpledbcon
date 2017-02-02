7cd1d913d5a448354a6db73a1908d4fb6f98c079
mwakhare <mwakhare@torinit.com>
2017-02-02 14:39:17 +0530

    update and insert problem

--- tblUserModule.js
+++ tblUserModule.js
@@ -262,77 +262,58 @@ tblUserInsert: function (newUserToAdd, callback)
      });
  },
 
-// var sql = "SELECT * FROM ?? WHERE ?? = ?";
-// var inserts = ['users', 'id', userId];
-// sql = mysql.format(sql, inserts);
 
-
-// var user = { id: 26, user_info: '{"Name":"sggg","Email":"gg@gmail.com","Password":"gg45kkk","DOB Date of Anniversary":"03/10/2017","Gender":"female","Social":"xp11","TC":"false","Mobile Number":"8754123658","Verified":"true","Active":"true","Last Login":"03/06/2017","IP Address":"192.168.1.125","MAC ID":"1589fds","Browser String":"yrytorrs"}'};
-
-// connection.query ("INSERT INTO user SET ?", user, function (err, result)
-
-
-// tblUserInsert: function  (user)
-//  {
-//     connection.query ("INSERT INTO user SET ?", user, 
-//     function (err, rows)
-//     {
-//         connection.release ();
-        
-//         if (err) 
-//         {
-//             console.error (err);
-//             return;
-//         }   
-
-//         if (!err) 
-//         {
-//             console.log ('Last insert ID: ' + rows.insertId);
-//             console.log (fields);            // fields contains extra meta data about results, if available
-//             return (true);
-//         }           
-//     });
-// },
+/*
+ * Function:  tblUserUpdate 
+ * -------------------------
+ * This function updates a new user recrod in the database
+ *
+ * userId: user id whose record is to update
+ * userUpdate: user update object to update in the database
+ * callback: callback async fuction 
+ *
+ * returns:  ?
+ *           ? returns zero on error or json (if record is not updated)
+ */
+tblUserUpdate: function (userId, userUpdate, callback)
+    {
+        pool.getConnection (function (err, connection)
+        {
+            if (err) 
+            {
+                connection.release ();
+                res.json ({"code" : 100, "status" : "Error in database connection"});  //dummy json error code (error code structure to fix)
+                return;
+            }   
  
+            console.log ('Update operation database connection thread id: ' + connection.threadId);
+         
+            var newUserToAddStringify =  JSON.stringify(newUserToAdd);
 
+            connection.query ('UPDATE user SET user_info = ? Where ID = ?',  [userUpdate, userId], function (err, rows, fields)
+            {
+                connection.release ();
+                if (err) 
+                {   
+                    console.error (err);
+                    callback (err);
+                    return;
+                }   
 
-// important running version
-
- //connection.query ('UPDATE user SET user_info = JSON_SET(user_info, "$.Name", ?,"$.Email",?) Where ID = ?', ["mohan","mn@gmail.com" ,24],function (err, result) 
-
-       
-
-// con.query(
-//   'UPDATE user SET user_info = JSON_SET(user_info, "$.Name", ?) Where ID = ?', ["VISHAL", 17],function (err, result) {
-//     if (err) throw err;
-//     console.log('Changed ' + result.changedRows + ' rows');
-//   }
-// );
-
-
-
-
-// tblUserUpdate: function  (id, user_info)
-//  {
-//     connection.query ("UPDATE user SET user_info = ? WHERE id = ?", [user_info, id], 
-//     function (err, rows, fields)
-//     {
-//         connection.release ();
-        
-//         if (err) 
-//         {
-//             console.error (err);
-//             return;
-//         }   
-
-//         if (!err) 
-//         {
-//             console.log ('\nChanged: ' + rows.changedRows + ' rows');
-//             console.log (fields);            // fields contains extra meta data about results, if available
-//             return (true);
-//         }           
-//     });
-// },
-
+                if (!err) 
+                {
+                    //console.log ('\nChanged: ' + rows.changedRows + ' rows');
+                    callback (null, rows, fields);
+                }           
+            });
+ 
+            //database connection error
+            connection.on ('error', function (err)
+            {      
+                res.json ({"code" : 100, "status" : "Error in database connection"}); //dummy json error code (error code structure to fix)
+                return;     
+            });
+     });
+ },
 
 }
